5fe077f26a199c0c886de081e0256505c2c34447
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-08-30 16:22:03 -0300

    mudança no index + mudança midleware erros

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { app } from "./app";
+import express from "express"
+import cors from "cors"
+import { AddressInfo } from "net"
 import { authRouter } from "./modules/auth/controller/auth.Routes";
 import { bookingRouter } from "./modules/booking/presentation/booking.Routes";
 import { calendarRouter } from "./modules/calendar/presentation/calender.Routes";
@@ -6,6 +8,22 @@ import { contractsRouter } from "./modules/contracts/presentation/contracts.Rout
 import { planRouter } from "./modules/plans/controller/Plan.Routes";
 import "express-async-errors";
 import { Request, Response } from "express";
+import { ZodError, ZodIssue } from "zod";
+import { CustomError } from "./common/customError/customError";
+
+export const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+   } else {
+      console.error(`Failure upon starting server.`);
+   }
+})
 
 app.use("/auth", authRouter  )
 // app.use("/contracts", contractsRouter )
@@ -15,5 +33,15 @@ app.use("/plans", planRouter )
 
 
 app.use((err:any, req: Request, res: Response, _:any) => {
-    res.status(err.statusCode || 500).send(err.message);
-  });
+  console.log(err)
+  
+  if (err instanceof ZodError) {
+    res.status(400).send(err.issues)
+  } else if (err instanceof Error) {
+    res.status(500).send(err.message)
+  } else if (err instanceof CustomError) {
+    res.status(err.statusCode).send(err.message)
+  } else {
+    res.status(500).send("Erro inesperado")
+  }
+});
