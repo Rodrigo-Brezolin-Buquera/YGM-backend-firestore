91f0c89e7efe08b3e064b8f5e443a043a7f6873f
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-09-01 11:53:22 -0300

    ajuste package.json + redução erros

--- src/common/customError/invalidRequests.ts
+++ src/common/customError/invalidRequests.ts
@@ -6,76 +6,15 @@ export class InvalidRequest extends CustomError {
   }
 }
 
-export class InvalidId extends CustomError {
-  constructor() {
-    super("O id precisa ser uma string válida", 400);
-  }
-}
-
-export class InvalidTime extends CustomError {
-  constructor() {
-    super("O horário deve ser no formato: `hh:mm`", 400);
-  }
-}
-
 export class InvalidDay extends CustomError {
   constructor() {
     super("Dia de aula inválido", 400);
   }
 }
 
-export class InvalidTeacher extends CustomError {
-  constructor() {
-    super("Os professores precisam ser 'Rodrigo' ou 'Louize'", 400);
-  }
-}
-
-export class InvalidName extends CustomError {
-  constructor() {
-    super(
-      "Nomes não podem caracteres especiais e números", 411);
-  }
-}
-
-export class InvalidClassQuantity extends CustomError {
-  constructor() {
-    super("O número de aulas disponíveis deve ser 0 ou maior", 400);
-  }
-}
-
-export class InvalidClassString extends CustomError {
-  constructor() {
-    super("O número de aulas para app e avulsas deve ser: --- ", 400);
-  }
-}
-
-export class InvalidDuration extends CustomError {
-  constructor() {
-    super("A duração do plano deve ser maior que zero", 400);
-  }
-}
-
-export class InvalidDate extends CustomError {
-  constructor() {
-    super("Data inválida, use o formato dd/mm/aaaa", 406);
-  }
-}
-
 export class InvalidInputDate extends CustomError {
   constructor() {
     super("Data inválida para requisição, use o formato YYYY-MM-DD", 406);
-  }
-}
-
-export class InvalidEmail extends CustomError {
-  constructor() {
-    super("Email inválido", 406);
-  }
-}
-
-export class InvalidPassword extends CustomError {
-  constructor() {
-    super("Senha inválida", 401);
   }
 }
 
@@ -112,47 +51,6 @@ export class InvalidYogaType extends CustomError {
     }
   }
 
-  export class InvalidVerified extends CustomError {
-    constructor() {
-      super(
-        `O campo verified precisa ser true ou false`,
-        400
-      );
-    }
-  }
 
-  export class InvalidEntity extends CustomError {
-    constructor() {
-      super(
-        `O parâmetro precisar ser "contract" ou "yogaClass"`,
-        400
-      );
-    }
-  }
-
-  export class InvalidAction extends CustomError {
-    constructor() {
-      super(
-        `O parâmetro action precisar ser "add" ou "subtract"`,
-        400
-      );
-    }
-  }
-
-  export class InvalidPayment extends CustomError {
-    constructor() {
-      super(
-        `O pagamento mensal precisa seguir o formato: "R$ 00,00"`,
-        400
-      );
-    }
-  }
-
-  export class InvalidCapacity extends CustomError {
-    constructor() {
-      super(
-        `A capacidade precisar ser um número maior que zero`,
-        400
-      );
-    }
-  }
+  
+  
--- src/common/domain/common.patterns.ts
+++ src/common/domain/common.patterns.ts
@@ -1,17 +1,17 @@
-import { InvalidDate, InvalidName, InvalidTime } from "../customError/invalidRequests";
+import { CustomError } from "../customError/customError";
 
 export const validateName = (name: string) => {
   const nameRegex: RegExp =
     /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
   if (!nameRegex.test(name)) {
-    throw new InvalidName();
+    throw new CustomError("Nomes não podem caracteres especiais e números", 400);
   }
 };
 
 export const validateDateFormat = (input: string) => {
   const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
   if (!dateRegex.test(input)) {
-    throw new InvalidDate();
+    throw new CustomError("Data inválida, use o formato dd/mm/aaaa", 400);
   }
 };
 
@@ -19,7 +19,7 @@ export const validateDateFormat = (input: string) => {
 export const validateTime = (time:string) => {
   const timeRegex = /^([01]\d|2[0-3]):[0-5]\d$/;
   if (!timeRegex.test(time)) {
-    throw new InvalidTime();
+    throw new CustomError("O horário deve ser no formato: `hh:mm`", 400);
   }
 }
 
