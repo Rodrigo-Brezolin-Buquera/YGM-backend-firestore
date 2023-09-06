6c3a62614750bd2457920ef327c06272432eebef
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-09-06 15:38:30 -0300

    testes common formatDate

--- src/common/utils/common.utils.formatDate.ts
+++ src/common/utils/common.utils.formatDate.ts
@@ -2,13 +2,17 @@ import { InvalidInputDate } from "../customError/invalidRequests";
 
 export const formatDate = (date: string) => {
   try {
-    const [year, month, day] = date.split("-");
+    const [year, month, day] = date.split("-").map(Number);
 
     if (!year || !month || !day) {
       throw new InvalidInputDate();
     }
 
-    return `${day.padStart(2, "0")}/${month.padStart(2, "0")}/${year}`;
+    if (year < 1000 || year > 9999 || month < 1 || month > 12 || day < 1 || day > 31) {
+      throw new InvalidInputDate()
+    }
+
+    return `${day.toString().padStart(2, "0")}/${month.toString().padStart(2, "0")}/${year}`;
   } catch (err) {
     throw new InvalidInputDate()
   }
--- tests/common/utils/common.utils.formatDate.test.ts
+++ tests/common/utils/common.utils.formatDate.test.ts
@@ -3,17 +3,17 @@ import { formatDate } from "../../../src/common/utils/common.utils.formatDate";
 
 
 describe("Common: FormatDate", () => {
-    const validFormat = ["2005-05-05", "2022-12-01"];
+    const validFormat = [{input:"2005-05-31", output:"31/05/2005"}, {input:"2022-12-01", output: "01/12/2022"}];
     validFormat.forEach((date) => {
-      test(`Valid date format ${date}`, () => {
-        const result = formatDate(date);
-        expect(result).toBeUndefined();
+      test(`Valid input date format ${date.input}`, () => {
+        const result = formatDate(date.input);
+        expect(result).toBe(date.output);
       });
     });
   
-    const invalidFormats = ["2022/05/15", "30/01", "01-10-2001", "May 5, 2022", "01-10-","-05-2022","35/02/1998", "/", "DD/MM/YYYY", "*" ];
+    const invalidFormats = ["2020-15-20", "2020-10-32", "2020-00-20", "2022/05/15", "30/01", "May 5, 2022", "01-10-","-05-2022","35/02/1998", "/", "DD/MM/YYYY", "*" ];
     invalidFormats.forEach((date) => {
-      test(`Invalid date format ${date}`, () => {
+      test(`Invalid input date format ${date}`, () => {
         expect.assertions(2);
         try {
           formatDate(date);
