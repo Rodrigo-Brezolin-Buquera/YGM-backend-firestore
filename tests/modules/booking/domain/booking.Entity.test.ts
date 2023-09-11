497851692f467c219b33a8a9315b6c09301a0552
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-09-11 16:57:34 -0300

    testes booking entity

--- tests/modules/booking/domain/booking.Entity.test.ts
+++ tests/modules/booking/domain/booking.Entity.test.ts
@@ -1,75 +1,32 @@
-// import { idText } from "typescript";
-// import { InvalidName, InvalidVerified } from "../../../../src/common/customError/invalidRequests";
-// import { Checkin } from "../../../../src/modules/booking/domain/booking.Entity";
+import { Checkin } from "../../../../src/modules/booking/domain/booking.Entity";
 
-// const getInitialObject = (): any => {
-//   return {
-//     id: "id",
-//     verified: true,
-//     name: "some name",
-//     date: "20/05/1989",
-//     classId: "id",
-//     contractId: "id"
-//   }
-// }
+const getInitialInput = (): any => {
+  return {
+    id: "id",
+    name: "some name",
+    date: "20/05/1989",
+    time: "19:00",
+    yogaClassId: "classId",
+    contractId: "contractId",
+  };
+};
 
-// const instanceOfCheckin = (obj: any): Checkin => {
-//   const result = new Checkin(obj.id, obj.name, obj.date, obj.contractId, obj.classId, obj.verified);
-//   return result;
-// }
-// describe("Sucess Tests on booking entity", () => {
-//   const obj = getInitialObject();
-//   test("Sucess case", () => {
-//     expect.assertions(1);
-//     try {
-//       const result = instanceOfCheckin(obj);
-//       expect(result).toBeInstanceOf(Checkin);
-//     } catch (error:any) {}
-//   });
-// });
+describe("Booking Entity", () => {
+  test("Sucess case", () => {
+    const input = getInitialInput();
+    const result = Checkin.toModel(input);
+    expect(result).toBeInstanceOf(Checkin);
+  });
 
-// describe("Fail name tests on booking entity", () => {
-//   const obj = getInitialObject();
-//   const currentError = new InvalidName();
-
-//   test("Invalid without name", () => {
-//     expect.assertions(3);
-//     obj.name = undefined;
-//     try {
-//       instanceOfCheckin(obj).checkName();
-//     } catch (error:any) {
-//       expect(error).toBeDefined();
-//       expect(error.message).toBe(currentError.message);
-//       expect(error.statusCode).toBe(currentError.statusCode);
-//     }
-//   });
-// });
-
-// describe("Fail verified tests on booking entity", () => {
-//   const obj = getInitialObject();
-//     const currentError = new InvalidVerified();
-  
-//     test("Invalid without verified", () => {
-//     expect.assertions(3);
-//       obj.verified = undefined;
-//       try {
-//         instanceOfCheckin(obj).checkVerified();
-//       } catch (error:any) {
-//         expect(error).toBeDefined();
-//         expect(error.message).toBe(currentError.message);
-//         expect(error.statusCode).toBe(currentError.statusCode);
-//       }
-//     });
-
-//     test("Invalid with string verified", () => {
-//         expect.assertions(3);
-//           obj.verified = "teste";
-//           try {
-//             const result = instanceOfCheckin(obj).checkVerified();
-//           } catch (error:any) {
-//             expect(error).toBeDefined();
-//             expect(error.message).toBe(currentError.message);
-//             expect(error.statusCode).toBe(currentError.statusCode);
-//           }
-//         });
-//   });
+  test("Sucess case: getters", () => {
+    const input = getInitialInput();
+    const result = Checkin.toModel(input);
+    console.log(result);
+    expect(result.getId()).toBe(input.id);
+    expect(result.getName()).toBe(input.name);
+    expect(result.getDate()).toBe(input.date);
+    expect(result.getTime()).toBe(input.time);
+    expect(result.getContractId()).toBe(input.contractId);
+    expect(result.getClassId()).toBe(input.yogaClassId);
+  });
+});
